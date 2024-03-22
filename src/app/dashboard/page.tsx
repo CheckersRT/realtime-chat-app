e498d4152e73240885d3b8eb27a2fa1cadc0e9f5
Tom Dent <tomdent991@hotmail.com>
2024-03-22 17:54:22 +0100

    first chapters complete

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/button"
+import {FC} from "react"
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+    return <Button variant="ghost">hello</Button>
+}
+
+export default page
